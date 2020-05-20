6d781b3b4a683dd2174372a155ccfc642befaf3b
Marco Perone <pasafama@gmail.com>
2020-05-20 15:32:49 +0200

    define Message

---
+++ src/Domain/Message.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tickets\Domain;
+
+final class Message
+{
+    /** @var Id<User> */
+    private $userId;
+
+    /** @var string */
+    private $body;
+
+    /**
+     * @param Id $userId
+     * @psalm-param Id<User> $userId
+     * @param string $body
+     */
+    private function __construct(Id $userId, string $body)
+    {
+        $this->userId = $userId;
+        $this->body = $body;
+    }
+
+    /**
+     * @param Id $userId
+     * @psalm-param Id<User> $userId
+     * @param string $body
+     * @return Message
+     */
+    public static function fromUserIdAndBody(Id $userId, string $body): self
+    {
+        return new self($userId, $body);
+    }
+}
