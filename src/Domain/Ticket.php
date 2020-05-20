0c491a5288d8ca7de80623c989f1f2d92b848308
Marco Perone <pasafama@gmail.com>
2020-05-20 15:17:07 +0200

    add Id to Ticket

--- src/Domain/Ticket.php
+++ src/Domain/Ticket.php
@@ -12,6 +12,12 @@ use Tickets\Domain\User\Admin;
  */
 final class Ticket
 {
+    /**
+     * @var Id
+     * @psalm-var Id<Ticket>
+     */
+    private $ticketId;
+
     /** @var \DateTimeImmutable */
     private $openedAt;
 
@@ -25,17 +31,23 @@ final class Ticket
     private $assignedTo;
 
     /**
+     * @param Id $ticketId
+     * @psalm-param Id<Ticket> $ticketId
      * @param \DateTimeImmutable $openedAt
      * @param \DateTimeImmutable $lastEditedAt
      * @param User $openedBy
+     * @param Maybe $assignedTo
      * @psalm-param Maybe<User<Admin>> $assignedTo
+     * @psalm-pure
      */
     private function __construct(
+        Id $ticketId,
         \DateTimeImmutable $openedAt,
         \DateTimeImmutable $lastEditedAt,
         User $openedBy,
         Maybe $assignedTo
     ) {
+        $this->ticketId = $ticketId;
         $this->openedAt = $openedAt;
         $this->lastEditedAt = $lastEditedAt;
         $this->openedBy = $openedBy;
