b20818c19606963d3565643bfc20b3f24fed9ae8
Marco Perone <pasafama@gmail.com>
2020-05-22 16:22:46 +0200

    tests on answer

--- spec/Domain/TicketSpec.php
+++ spec/Domain/TicketSpec.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace TicketsSpec\Domain;
 
 use Lcobucci\Clock\FrozenClock;
+use Marcosh\LamPHPda\Either;
 use Marcosh\LamPHPda\Maybe;
 use Ramsey\Uuid\Uuid;
 use Tickets\Domain\Id;
 use Tickets\Domain\Message;
 use Tickets\Domain\Ticket;
 use Tickets\Domain\User;
+use Tickets\Error\AnswerError;
+use Tickets\Event\NewAnswerArrived;
 use Tickets\Event\TicketOpened;
 
 describe('Ticket', function () {
@@ -65,5 +68,155 @@ describe('Ticket', function () {
         expect($ticket->assignedTo())->toEqual(Maybe::nothing());
         expect($ticket->messages())->toBe([$message]);
         expect($ticket->status())->toEqual(Ticket\Status::new());
+    });
+
+    it('emits NewAnswerArrived when answer arrives from admin', function () {
+        $ticketId = Id::fromUuid(Uuid::uuid4());
+        $user = User::withIdAndProfile(
+            Id::fromUuid(Uuid::uuid4()),
+            new User\Common()
+        );
+        $message = Message::fromUserAndBody($user, 'a message');
+        $then = date_create_immutable();
+
+        $ticket = Ticket::onTicketOpened(
+            new TicketOpened(
+                $ticketId,
+                $message,
+                $then
+            )
+        );
+
+        $admin = User::withIdAndProfile(
+            Id::fromUuid(Uuid::uuid4()),
+            new User\Admin()
+        );
+        $answerMessage = Message::fromUserAndBody($admin, 'an answer');
+
+        $now = date_create_immutable();
+        $clock = new FrozenClock($now);
+
+        $events = $ticket->answer($answerMessage, $clock);
+
+        expect($events)->toEqual(Either::right([
+            new NewAnswerArrived(
+                $ticketId,
+                $answerMessage,
+                $now
+            )
+        ]));
+    });
+
+    it('emits NewAnswerArrived when answer arrives from user who opened the ticket', function () {
+        $ticketId = Id::fromUuid(Uuid::uuid4());
+        $user = User::withIdAndProfile(
+            Id::fromUuid(Uuid::uuid4()),
+            new User\Common()
+        );
+        $message = Message::fromUserAndBody($user, 'a message');
+        $then = date_create_immutable();
+
+        $ticket = Ticket::onTicketOpened(
+            new TicketOpened(
+                $ticketId,
+                $message,
+                $then
+            )
+        );
+
+        $answerMessage = Message::fromUserAndBody($user, 'an answer');
+
+        $now = date_create_immutable();
+        $clock = new FrozenClock($now);
+
+        $events = $ticket->answer($answerMessage, $clock);
+
+        expect($events)->toEqual(Either::right([
+            new NewAnswerArrived(
+                $ticketId,
+                $answerMessage,
+                $now
+            )
+        ]));
+    });
+
+    it('returns an error when answer arrives from user who didn\'t open the ticket', function () {
+        $ticketId = Id::fromUuid(Uuid::uuid4());
+        $user = User::withIdAndProfile(
+            Id::fromUuid(Uuid::uuid4()),
+            new User\Common()
+        );
+        $message = Message::fromUserAndBody($user, 'a message');
+        $then = date_create_immutable();
+
+        $ticket = Ticket::onTicketOpened(
+            new TicketOpened(
+                $ticketId,
+                $message,
+                $then
+            )
+        );
+
+        $anotherUser = User::withIdAndProfile(
+            Id::fromUuid(Uuid::uuid4()),
+            new User\Common()
+        );
+        $answerMessage = Message::fromUserAndBody($anotherUser, 'an answer');
+
+        $now = date_create_immutable();
+        $clock = new FrozenClock($now);
+
+        $error = $ticket->answer($answerMessage, $clock);
+
+        expect($error)->toEqual(Either::left(AnswerError::userCanAnswerOnlyToTicketsHeCreated()));
+    });
+
+    it('returns an error when answer arrives from an admin who is not the assignee', function () {
+        $ticketId = Id::fromUuid(Uuid::uuid4());
+        $user = User::withIdAndProfile(
+            Id::fromUuid(Uuid::uuid4()),
+            new User\Common()
+        );
+        $message = Message::fromUserAndBody($user, 'a message');
+        $then = date_create_immutable();
+
+        $ticket = Ticket::onTicketOpened(
+            new TicketOpened(
+                $ticketId,
+                $message,
+                $then
+            )
+        );
+
+        $admin = User::withIdAndProfile(
+            Id::fromUuid(Uuid::uuid4()),
+            new User\Admin()
+        );
+        $answerMessage = Message::fromUserAndBody($admin, 'an answer');
+
+        $now = date_create_immutable();
+        $clock = new FrozenClock($now);
+
+        $eitherEvents = $ticket->answer($answerMessage, $clock);
+
+        $eitherEvents->eval(
+            function ($error) {expect(1)->toBe(2);},
+            function ($events) use ($ticket) {
+                $ticket = $ticket->onNewAnswerArrived($events[0]);
+
+                $anotherAdmin = User::withIdAndProfile(
+                    Id::fromUuid(Uuid::uuid4()),
+                    new User\Admin()
+                );
+                $answerMessage = Message::fromUserAndBody($anotherAdmin, 'an answer');
+
+                $now = date_create_immutable();
+                $clock = new FrozenClock($now);
+
+                $error = $ticket->answer($answerMessage, $clock);
+
+                expect($error)->toEqual(Either::left(AnswerError::adminCanAnswerOnlyNewOrAssignedTickets()));
+            }
+        );
     });
 });
