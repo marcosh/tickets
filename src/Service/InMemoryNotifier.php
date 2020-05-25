1887a35634171fd840586f4b82ff99a58eb0f9f3
Marco Perone <pasafama@gmail.com>
2020-05-25 09:39:17 +0200

    in memory notifier

---
+++ src/Service/InMemoryNotifier.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tickets\Service;
+
+use Tickets\Domain\Ticket;
+use Tickets\Domain\User;
+
+final class InMemoryNotifier implements Notifier
+{
+    /** @var array{user: User, ticket: Ticket}[] */
+    private $notifications = [];
+
+    /**
+     * @param User[] $users
+     * @param Ticket $ticket
+     */
+    public function notifyUsersOfTicket(array $users, Ticket $ticket): void
+    {
+        foreach ($users as $user) {
+            $this->notifications[] = [
+                "user" => $user,
+                "ticket" => $ticket
+            ];
+        }
+    }
+
+    public function notifications(): array
+    {
+        return $this->notifications;
+    }
+}
